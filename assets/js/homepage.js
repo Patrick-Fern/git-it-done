84628612e46ba3ca2552a377343b66733d5f1dc8
patfern <patrick.j.fernbach@gmail.com>
2022-01-21 19:52:26 -0500

    request data script

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,5 +1,12 @@
-var getUserRepos = function() {
-    fetch("https://api.github.com/users/octocat/repos");
+var getUserRepos = function(user) {
+    var apiUrl = "https://api.github.com/users/facebook/repos";
+
+    //make a request to the url
+    fetch(apiUrl).then(function(respone){
+        respone.json().then(function(data){
+            console.log(data);
+        });
+    });
 };
 
 getUserRepos();
