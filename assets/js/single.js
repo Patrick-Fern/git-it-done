92db516ff913c3ebe2822182b11220e912b81d6a
patfern <patrick.j.fernbach@gmail.com>
2022-01-24 22:16:15 -0500

    more issue warning

--- assets/js/single.js
+++ assets/js/single.js
@@ -1,4 +1,5 @@
 var issuesContainerEl = document.querySelector("#issues-container");
+var limitWarningEl = document.querySelector("#limit-warning");
 
 var displayIssues = function(issues) {
     if (issues.length === 0) {
@@ -33,6 +34,18 @@ var displayIssues = function(issues) {
 
 };
 
+var displayWarning = function(repo) {
+    // add text to warning container
+    limitWarningEl.textContent = "To see more than 30 issues, visit ";
+
+    var linkEl = document.createElement("a");
+    linkEl.textContent = "See More Issues on GitHub.com";
+    linkEl.setAttribute("href", "https://github.com/" + repo + "/issues");
+    linkEl.setAttribute("target", "_blank");
+
+    limitWarningEl.appendChild(linkEl);
+};
+
 
 var getRepoIssues = function(repo) {
     var apiUrl = "https://api.github.com/repos/" + repo + "/issues?direction=asc";
@@ -42,6 +55,11 @@ var getRepoIssues = function(repo) {
         if (response.ok) {
             response.json().then(function(data) {
                 displayIssues(data);
+
+                // check if api has paginated issues
+                if (response.headers.get("Link")) {
+                    displayWarning(repo);
+                }
             });
         } 
         else {
@@ -52,4 +70,4 @@ var getRepoIssues = function(repo) {
 
 
 
-getRepoIssues("Patrick-Fern/run-buddy");
+getRepoIssues("Facebook/react");
